f7c7d8ecc71628812fd4d82e3092705345140b1f
hhhryoma <poipoi.rh@gmail.com>
2020-12-20 02:34:56 +0900

    test-react-router

---
+++ test-react-router/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Layout from "./pages/Layout";
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Layout />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
