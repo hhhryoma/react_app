b830df4f38e85a760a64859dcc1fa513bf764cf8
hhhryoma <poipoi.rh@gmail.com>
2020-12-27 00:02:46 +0900

    add filter

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -4,9 +4,27 @@ import Todo from "./components/Todo"
 import Form from "./components/Form"
 import FilterButton from "./components/FilterButton"
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+}
+
+const FILTER_NAMES = Object.keys(FILTER_MAP)
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks)
+  const [filter, setFilter] = useState('All')
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton 
+      key={name} 
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ))
+
+  console.log(`test: ${filterList}, ${FILTER_NAMES}`)
 
   function addTask(name) {
     const newTask = { id: "todo-" + nanoid(), name: name, completed: false}
@@ -38,7 +56,7 @@ function App(props) {
     setTasks(updatedTasks)
   }
 
-  const taskList = tasks.map(task => (
+  const taskList = tasks.filter(FILTER_MAP[filter]).map(task => (
     <Todo id={task.id} 
           name={task.name} 
           completed={task.completed} 
@@ -57,9 +75,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton></FilterButton>
-        <FilterButton></FilterButton>
-        <FilterButton></FilterButton>
+        {filterList}
       </div>
       <h2 id="list-heading">
         {headingText}
